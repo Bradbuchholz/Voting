172fc2ec3d6d92e3c05ad81d83023ab742bf2790
Bradbuchholz <bradbuchholz@gmail.com>
2022-04-05 19:46:25 -0700

    add alert prompts

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
   const age = parseInt(prompt("How old are you?"));
 
-  if (age >=18) {
+  if (age >18) {
+    alert("You're old enough to vote!");
+    $('#voting').show();
+  } else if (age === 18) {
+    alert("You're old enough to vote!");
     $('#voting').show();
   } else {
+    alert("You're not old enough to vote");
     $('#under-18').show();
   }
 });
